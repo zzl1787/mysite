0fa849ba775c5ca8c52df82730c8f3d68a291236
zhaozhe <178730210@qq.com>
2017-05-06 12:28:24 +0800

    hot loader

--- src/components/MainLayout/Header.js
+++ src/components/MainLayout/Header.js
@@ -9,14 +9,14 @@ function Header({ location }) {
       mode="horizontal"
       theme="dark"
     >
-      <Menu.Item key="/users">
-        <Link to="/users"><Icon type="bars" />Users</Link>
-      </Menu.Item>
       <Menu.Item key="/">
         <Link to="/"><Icon type="home" />Home</Link>
       </Menu.Item>
+      <Menu.Item key="/users">
+        <Link to="/users"><Icon type="bars" />Users</Link>
+      </Menu.Item>
       <Menu.Item key="/404">
-        <Link to="/page-you-dont-know"><Icon type="frown-circle" />404</Link>
+        <Link to="/page-you-dont-know"><Icon type="smile-o" />404</Link>
       </Menu.Item>
       <Menu.Item key="/antd">
         <a href="https://github.com/dvajs/dva">dva</a>
